3aa88fad13c6b47e8b77f66fda89580f8440c891
mxhey <mhey0706@sydney.uni.edu.au>
2023-10-25 18:31:08 +1100

    added circle generation with randomisation

--- sketch.js
+++ sketch.js
@@ -1,32 +1,52 @@
 let x;
 let y;
-let diam;
-let circle2; 
+let diam = 350;
+let circle2;
 let max = 15;
+
+let circles = [];
+let finishedCircles = [];
+let overlap = false;
+let yPadding = 300;
+let xPadding = 250;
+let tempX;
+let tempY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = random(width);
-  y = random(height);
-  diam = random(250, 350)
-  circle2 = diam - random(150, 200);
+
   colorMode(HSB);
   background(197, 100, 46);
-  
-  let circle = new Circle(x, y, diam, circle2);
-  circle.draw();
 
-  let dots = new Dots(x, y, diam, circle2);
-  dots.draw();
- 
+  for (i = 0; i < max; i++) {
+    y = (75 + yPadding) * i;
 
+    for (j = 0; j < 6; j++) {
+      y += +50;
+      x = (150 + xPadding) * j + random(-40, 40);
+      //SET DIAMETER AND RANDOM INNER CIRCLE DIAMETER
+
+      circle2 = diam - random(150, 200);
+
+      let circle = new Circle(x, y, diam, circle2);
+
+      circles.push(circle);
+    }
+  }
+
+  for (j = 0; j < circles.length; j++) {
+    circles[j].draw();
+    let dots = new Dots(
+      circles[j].xPos,
+      circles[j].yPos,
+      circles[j].diam,
+      circles[j].circle2
+    );
+    dots.draw();
+  }
 }
 
-function draw() {
-
-  //ellipse(width/2, height/2, 50);
-
-}
+function draw() {}
 
 class Circle {
   constructor(x, y, d, c2) {
@@ -59,36 +79,35 @@ class Circle {
 
     fill(this.color4);
     ellipse(this.xPos, this.yPos, this.circle4);
-
   }
 }
 
 class Dots {
   constructor(x, y, diam, min) {
-    
     this.x = x;
     this.y = y;
     this.diam = diam;
-    this.min = min
+    this.min = min;
 
     this.noOfDots = 40;
     this.noOfLayers = (this.diam - this.min) / 50;
   }
 
   draw() {
+    push();
     noStroke();
     fill(random(360), 85, 30);
-    push()
-    translate(x, y);
+    push();
+    translate(this.x, this.y);
     let size = 20;
 
-  for (let j = 0; j < this.noOfLayers; j++) {
-    for (let i = 0; i < this.noOfDots; i++) {
-      
-      ellipse((this.min/2 + size/2) + j * size, 0, size);
-      rotate(i * this.noOfDots)
+    for (let j = 0; j < this.noOfLayers; j++) {
+      for (let i = 0; i < this.noOfDots; i++) {
+        ellipse(this.min / 2 + size / 2 + j * size, 0, size);
+        rotate(i * this.noOfDots);
+      }
     }
-  }
-    
+
+    pop();
   }
 }
