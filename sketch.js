4b199daceead82df0fe533d687e9b5e0167f9863
mxhey <mhey0706@sydney.uni.edu.au>
2023-10-27 15:04:54 +1100

    experiment with random agents

--- sketch.js
+++ sketch.js
@@ -5,18 +5,26 @@ let circle2;
 let max = 15;
 
 let circles = [];
-let finishedCircles = [];
+let dots = [];
 let overlap = false;
 let yPadding = 300;
 let xPadding = 250;
 let tempX;
 let tempY;
 
+let agents = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   colorMode(HSB);
   background(197, 100, 46);
+
+  for (let i = 0; i < width; i+=250){
+    for (let j = 0; j < height; j+=250){
+      agents.push(new Agent(i,j));
+    }
+  }5
 
   for (i = 0; i < max; i++) {
     y = (75 + yPadding) * i;
@@ -31,22 +39,30 @@ function setup() {
       let circle = new Circle(x, y, diam, circle2);
 
       circles.push(circle);
+
+      let dot = new Dot(x, y, diam, circle2);
+
+      dots.push(dot);
     }
   }
 
   for (j = 0; j < circles.length; j++) {
     circles[j].draw();
-    let dots = new Dots(
-      circles[j].xPos,
-      circles[j].yPos,
-      circles[j].diam,
-      circles[j].circle2
-    );
-    dots.draw();
+    
+    dots[j].draw();
   }
 }
 
-function draw() {}
+function draw() {
+  updateAgents();
+  sortAgentsBySize(agents);
+
+  for (j = 0; j < circles.length; j++) {
+    circles[j].draw();
+    dots[j].draw();
+  }
+}
+
 
 class Circle {
   constructor(x, y, d, c2) {
@@ -82,12 +98,13 @@ class Circle {
   }
 }
 
-class Dots {
+class Dot {
   constructor(x, y, diam, min) {
     this.x = x;
     this.y = y;
     this.diam = diam;
     this.min = min;
+    this.color = color(random(360), 85, 30);
 
     this.noOfDots = 40;
     this.noOfLayers = (this.diam - this.min) / 50;
@@ -96,7 +113,7 @@ class Dots {
   draw() {
     push();
     noStroke();
-    fill(random(360), 85, 30);
+    fill(this.color);
     push();
     translate(this.x, this.y);
     let size = 20;
@@ -110,4 +127,68 @@ class Dots {
 
     pop();
   }
+}
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+function sortAgentsBySize(array){
+  for (var i = 0; i < array.length-1; i++){
+    if (array[i].r < array[i+1].r){
+      var temp = array[i];
+      array[i] = array[i+1];
+      array[i+1] = temp;
+      //i = 0;
+    }
+  }
+}
+
+
+function updateAgents(){
+  if(true){
+    for (let i = 0; i < agents.length; i++){
+      agents[i].update();
+    }
+  }else{
+    for (let i = 0; i < agents.length/4; i++){
+      agents[i].update();
+    }
+
+  }
+}
+class Agent{
+  constructor(x, y){
+    this.x = x; this.y = y; this.r = 20;
+    this.xv = 10;
+    this.yv = 10;
+    this.first = true;
+  }
+  update(){
+    translate(circle.xPos, 250)
+    fill(random(360), 85, 55, );
+    if(this.first){
+      for(let i = 0; i < 50; i+=5){
+        circle(this.x,this.y,i);
+      }
+      noFill();stroke(0,10);circle(this.x,this.y,100);noStroke();
+      this.first = false;
+    }
+    else{
+      
+      circle(this.x,this.y,this.r);
+      this.x+=random(-this.xv,this.xv);this.y+=random(-this.yv,this.yv);
+      this.xv+=random(-.1,.1);
+      this.yv+=random(-.1, .1)
+      this.r+=random(-.1,.1);
+    
+      if (this.x > width)this.x = width; if (this.x <= 0)this.x = 0;
+      if (this.y > height)this.y = height; if (this.y <=0)this.y = 0;
+    }
+  }
+
 }
